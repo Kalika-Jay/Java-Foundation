5edcb8097928254f264daaa421994cba5b1192f8
Kalika Jayasinghearachchi <kalika.jay2004@gmail.com>
2025-08-05 11:44:20 +0530

    Variables added

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,7 +1,21 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int hours = 0;
+        System.out.println("How many hours did you work?");
+        Scanner input = new Scanner(System.in);
+        hours = input.nextInt();
+
+        double pay_rate = 0;
+        System.out.println("What is your pay rate?");
+        pay_rate = input.nextDouble();
+
+        input.close();
+
+        double grossPay = hours * pay_rate;
+        System.out.println("Gross pay: "+grossPay);
     }
 }
