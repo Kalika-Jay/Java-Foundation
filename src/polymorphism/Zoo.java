979836bacb23dd6b5f920a7fc14dc3e487c6c42a
Kalika Jayasinghe Arachchi <kalika.jay2004@gmail.com>
2025-08-07 10:03:16 +0530

    Polymorphism added

---
+++ src/polymorphism/Zoo.java
@@ -0,0 +1,28 @@
+package polymorphism;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Dog rocky =  new Dog();
+        rocky.fetch();
+        rocky.makeSound();
+        feed(rocky);
+
+        Animal sasha = new Dog();
+        sasha.makeSound();
+
+        sasha = new Cat();
+        sasha.makeSound();
+
+        ((Cat)sasha).scratch();
+        feed(sasha);
+    }
+
+    public static void feed(Animal animal){
+        if(animal instanceof Dog){
+            System.out.println("Dog food");
+        }
+        else if(animal instanceof Cat){
+            System.out.println("Cat food");
+        }
+    }
+}
